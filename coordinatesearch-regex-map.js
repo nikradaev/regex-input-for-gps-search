70721f2111ca49434f18f2582d1e4ce97f6d9607
Nik Radaev <nik.radaev@gmail.com>
2018-01-12 09:52:28 -0700

    Update coordinatesearch-regex-map.js

--- coordinatesearch-regex-map.js
+++ coordinatesearch-regex-map.js
@@ -1,63 +1,64 @@
-                // implemented handling different patterns for coordinates search
-                
-                me.key = "coordinate";
+// implemented handling different patterns for coordinates search
 
-                me.search = function (textSearchString, extent) {
-                    //  Here we are testing for a lat lng pattern match.  This will need to be changed or extended in the future.
-                    var matches = textSearchString.match(/^(\-?\d+(\.\d+)?)[,/: ]\s*(\-?\d+(\.\d+)?)$/);
-                    if (matches == null) {
+me.key = "coordinate";
 
-                    // removing (chopping off) the whitespaces or other characters from end of line until hit the first digit, so we can have the second argument on the right place / match[7] / 
-                    for (i = textSearchString.length - 1; i > 0; i--) {
-                        if (textSearchString[i].match(/^\d$/)) break;
-                        if (textSearchString[i].match(/^[\s\;\,\°]+$/)) textSearchString = textSearchString.slice(0, i) + textSearchString.slice(i + 1, textSearchString.length);
-                    }
+me.search = function (textSearchString, extent) {
+	//  Here we are testing for a lat lng pattern match.  This will need to be changed or extended in the future.
+	var matches = textSearchString.match(/^(\-?\d+(\.\d+)?)[,/: ]\s*(\-?\d+(\.\d+)?)$/);
+	if (matches == null) {
 
-                    // Regular expression pattern as following:
-                    // -+digit(with .) whitespace-+ -+digit(with .)(') -+digit(with .)(") A-Za-z [whitespace / delimeter] -+digit(with .) whitespace-+ -+digit(with .)(') -+digit(with .)(") A-Za-z whitespace
-                    var decExpr = /^(\-?\+?\d*(?:\.\d*)?(?:\s*))(\-?\+?\d*(?:\.\d*)?)(\'*)(?:\s*)(\-?\+?\d*(?:\.\d*)?)(\"*)(?:\s*)([A-Za-z]*)[\,\:\;\s\ ]+(\-?\+?\d*(?:\.\d*)?(?:\s*))(\-?\+?\d*(?:\.\d*)?)(\'*)(?:\s*)(\-?\+?\d*(?:\.\d*)?)(\"*)(?:\s*)([A-Za-z]*)$/;
+		// removing (chopping off) the whitespaces or other characters from end of line until hit the first digit, so we can have the second argument on the right place / match[7] / 
+		for (i = textSearchString.length - 1; i > 0; i--) {
+			if (textSearchString[i].match(/^\d$/)) break;
+			if (textSearchString[i].match(/^[\s\;\,\°]+$/)) textSearchString = textSearchString.slice(0, i) + textSearchString.slice(i + 1, textSearchString.length);
+		}
 
-                    // match the regex
-                    var match = textSearchString.match(decExpr);
-                   
-                    // if null - pass null
-                    if (match == null) {
-                        self.appViewModel.resultsViewModel.coordinateSearchResult(null);
-                        return;
-                    }
+		// Regular expression pattern as following:
+		// -+digit(with .) whitespace-+ -+digit(with .)(') -+digit(with .)(") A-Za-z [whitespace / delimeter] -+digit(with .) whitespace-+ -+digit(with .)(') -+digit(with .)(") A-Za-z whitespace
+		var decExpr = /^(\-?\+?\d*(?:\.\d*)?(?:\s*))(\-?\+?\d*(?:\.\d*)?)(\'*)(?:\s*)(\-?\+?\d*(?:\.\d*)?)(\"*)(?:\s*)([A-Za-z]*)[\,\:\;\s\ ]+(\-?\+?\d*(?:\.\d*)?(?:\s*))(\-?\+?\d*(?:\.\d*)?)(\'*)(?:\s*)(\-?\+?\d*(?:\.\d*)?)(\"*)(?:\s*)([A-Za-z]*)$/;
 
-                    var point = textSearchString.match(/-?\d+(\.\d+)?/g);
-                    // handling the signs (+-) 
-                    var latSign = Math.sign(match[1] * 1);
-                    if (latSign == 0 || latSign == -0) latSign = 1;
+		// match the regex
+		var match = textSearchString.match(decExpr);
 
-                    var lngSign = Math.sign(match[7] * 1);
-                    if (lngSign == 0 || lngSign == -0) lngSign = 1;
+		// if null - pass null
+		if (match == null) {
+			self.appViewModel.resultsViewModel.coordinateSearchResult(null);
+			return;
+		}
 
-                    var latSignAdj = 1;
-                    var lngSignAdj = 1;
+		// handling the signs (+-)
+		var point = textSearchString.match(/-?\d+(\.\d+)?/g);
+		var latSign = Math.sign(match[1] * 1);
+		if (latSign == 0 || latSign == -0) latSign = 1;
 
-                    // coefficients for minutes and seconds
-                    var latMinMul = 1 / 60;
-                    var lngMinMul = 1 / 60;
+		var lngSign = Math.sign(match[7] * 1);
+		if (lngSign == 0 || lngSign == -0) lngSign = 1;
 
-                    var latSecMul = 1 / 3600;
-                    var lngSecMul = 1 / 3600;
+		var latSignAdj = 1;
+		var lngSignAdj = 1;
 
-                    // reverting signs if hit 's' or 'w'
-                    if (match[6].charAt(0).toLowerCase() == 's') {
-                        latSign = latSign * -1;
-                        latSignAdj = -1;
-                    }
+		// coefficients for minutes and seconds
+		var latMinMul = 1 / 60;
+		var lngMinMul = 1 / 60;
 
-                    if (match[12].charAt(0).toLowerCase() == 'w') {
-                        lngSign = lngSign * -1;
-                        lngSignAdj = -1;
-                    }
+		var latSecMul = 1 / 3600;
+		var lngSecMul = 1 / 3600;
 
-                    // creating an array with proper decimal coordinates and correct signs (-+)
-                    var point = [latSignAdj * match[1] + latSign * (latMinMul * match[2] + latSecMul * match[4]),
-                        lngSignAdj * match[7] + lngSign * (lngMinMul * match[8] + lngSecMul * match[10])];
+		// reverting signs if hit 's' or 'w'
+		if (match[6].charAt(0).toLowerCase() == 's') {
+			latSign = latSign * -1;
+			latSignAdj = -1;
+		}
 
-                    var latLng = self.appViewModel.mapViewModel.createLatLong(point[0], point[1]);
-                    self.appViewModel.resultsViewModel.coordinateSearchResult(latLng);
+		if (match[12].charAt(0).toLowerCase() == 'w') {
+			lngSign = lngSign * -1;
+			lngSignAdj = -1;
+		}
+
+		// creating an array with proper decimal coordinates and correct signs (-+)
+		var point = [latSignAdj * match[1] + latSign * (latMinMul * match[2] + latSecMul * match[4]),
+		lngSignAdj * match[7] + lngSign * (lngMinMul * match[8] + lngSecMul * match[10])];
+
+		var latLng = self.appViewModel.mapViewModel.createLatLong(point[0], point[1]);
+		self.appViewModel.resultsViewModel.coordinateSearchResult(latLng);
+}
